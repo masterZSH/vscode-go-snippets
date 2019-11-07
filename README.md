2bfd72396033c841f8685ecf97162d4cd8b7824a
zsh <shaohua.zhou@joojtech.com>
2019-11-07 15:05:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## Usage
+|prefix|description|
+|---|---|
+|package|declare go package|
+|import-m|multiple packages|
+|import-s|single packages|
+|func|function|
+|main|main function|
---
+++ src/updateReadme.js
@@ -0,0 +1,89 @@
+/**
+ * 更新README.md文件
+ * @summary 更新readme文件
+ */
+
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const snippetsFilePath = path.resolve(__dirname, './snippets/go.json');
+const mdFilePath = path.resolve(__dirname, '../README.md');
+process.on('uncaughtException', function (err) {
+    console.log('Caught Exception:' + err);
+});
+
+/**
+ * 更新主方法
+ */
+function update() {
+    let content = getContent(snippetsFilePath);
+    let obj = JSON.parse(content);
+    let entries = Object.entries(obj);
+    let mdContent = "";
+    mdContent += getHeader();
+    mdContent += getTableHeader();
+    mdContent += getTableContent();
+    entries.map(v => {
+        mdContent += getTrContent(v[1].prefix, v[1].description);
+    });
+    let writeStream = fs.createWriteStream(mdFilePath, {
+        flags: 'w+',
+        encoding: "utf8"
+    });
+    writeStream.write(mdContent, 'utf8');
+    writeStream.on('error', function (err) {
+        console.log(err);
+    });
+    writeStream.end();
+}
+
+/**
+ * 获取文件内容
+ * @param {string} filePath -文件地址
+ * @returns {string} -返回的内容
+ */
+function getContent(filePath) {
+    let snippetsFilePath = filePath;
+    if (!fs.existsSync(snippetsFilePath)) {
+        throw new RangeError("no file exists");
+    }
+    let contentBuffer = fs.readFileSync(snippetsFilePath, {
+        encoding: 'utf8'
+    });
+    let content = contentBuffer.toString();
+    return content;
+}
+
+
+
+/**
+ * 获取头部
+ */
+function getHeader() {
+    return "## Usage\r\n";
+}
+
+/**
+ * 获取table头部
+ */
+function getTableHeader() {
+    return '|prefix|description|\r\n';
+}
+
+/**
+ * 获取table内容
+ */
+function getTableContent() {
+    return "|---|---|\r\n";
+}
+
+/**
+ * 获取table tr内容
+ * @param {string} prefix -前置
+ * @param {string} description -描述
+ */
+function getTrContent(prefix, description) {
+    return `|${prefix}|${description}|\r\n`;
+}
+
+update();
